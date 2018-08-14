72f41dbfa2611cdda46a95ad057448999f3171d9
chen.zhang <chen.zhang@rokid.com>
2018-08-14 21:08:10 +0800

    vsys编译libcaps, libmisc

--- ndk.mk
+++ ndk.mk
@@ -22,3 +22,49 @@ LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include/log
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/log
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcaps
+LOCAL_LDLIBS := -llog
+
+MY_FILES_PATH := $(LOCAL_PATH)/src/caps
+MY_FILES_SUFFIX := %.cpp %.c %.cc
+My_All_Files := \
+	$(patsubst \
+		$(LOCAL_PATH)/%, \
+		%, \
+		$(filter \
+			$(MY_FILES_SUFFIX), \
+			$(shell \
+				find $(MY_FILES_PATH) -type f \
+			) \
+		) \
+	)
+LOCAL_SRC_FILES := $(My_All_Files)
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/include/caps
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/caps
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmisc
+LOCAL_LDLIBS := -llog
+
+MY_FILES_PATH := $(LOCAL_PATH)/src/misc
+MY_FILES_SUFFIX := %.cpp %.c %.cc
+My_All_Files := \
+	$(patsubst \
+		$(LOCAL_PATH)/%, \
+		%, \
+		$(filter \
+			$(MY_FILES_SUFFIX), \
+			$(shell \
+				find $(MY_FILES_PATH) -type f \
+			) \
+		) \
+	)
+LOCAL_SRC_FILES := $(My_All_Files)
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/include/misc
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/misc
+include $(BUILD_STATIC_LIBRARY)
