04c27e844eb167aab5075e6da3268c4b08b96b54
chen.zhang <chen.zhang@rokid.com>
2018-12-13 18:10:53 +0800

    Revert "reimplement clargs"
    This reverts commit 6879b57e839dbf7de2ae35b9c86062e034af3d1e.

--- demo/misc/clargs_demo.c
+++ demo/misc/clargs_demo.c
@@ -1,9 +1,6 @@
 #include <string.h>
 #include <stdio.h>
-#include <memory>
 #include "clargs.h"
-
-using namespace std;
 
 typedef struct {
 	int argc;
@@ -39,26 +36,33 @@ const char* visible_string(int c, char** v) {
 	return buf;
 }
 
-void display_parse_result(shared_ptr<CLArgs>& handle) {
+void display_parse_result(clargs_h handle) {
 	const char* key;
 	const char* value;
-  uint32_t i;
-  uint32_t size = handle->size();
 
-	printf("[pairs key value]\n");
-  for (i = 0; i < size; ++i) {
-		printf("\t%s: %s\n", (*handle)[i].key, (*handle)[i].value);
+	printf("[options]\n");
+	while (1) {
+		if (clargs_opt_next(handle, &key, &value))
+			break;
+		printf("\t%s: %s\n", key, value);
+	}
+
+	printf("[args]\n");
+	while (1) {
+		if (clargs_arg_next(handle, &value))
+			break;
+		printf("\t%s\n", value);
 	}
 }
 
 int main(int argc, char** argv) {
 	size_t count = sizeof(test_args) / sizeof(CmdLineArgs);
 	size_t i;
-  shared_ptr<CLArgs> handle;
+	clargs_h handle;
 
 	for (i = 0; i < count; ++i) {
-		handle = CLArgs::parse(test_args[i].argc, test_args[i].argv);
-		if (handle == nullptr) {
+		handle = clargs_parse(test_args[i].argc, test_args[i].argv);
+		if (handle == 0) {
 			printf("parse %s failed.\n", visible_string(test_args[i].argc, test_args[i].argv));
 			break;
 		}
--- src/misc/clargs.cc
+++ src/misc/clargs.cc
@@ -1,225 +1,199 @@
-#include <sys/mman.h>
 #include <string.h>
-#include <stdlib.h>
 #include <cctype>
-#include <vector>
+#include <map>
+#include <list>
+#include <string>
 #include "clargs.h"
 
-static const uint32_t BUFFER_LENGTH = 1024 * 1024;
+using std::map;
+using std::list;
+using std::string;
 
-using namespace std;
-
-class clargs_inst : public CLArgs {
+class clargs_inst {
 public:
-  clargs_inst() {
-    clpairs.reserve(16);
-    buffer = (char*)mmap(nullptr, BUFFER_LENGTH, PROT_READ | PROT_WRITE,
-        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    empty_pair.key = nullptr;
-    empty_pair.value = nullptr;
-  }
-
-  ~clargs_inst() {
-    munmap(buffer, BUFFER_LENGTH);
-  }
-
-  uint32_t size() const {
-    return clpairs.size();
-  }
-
-  const CLPair& operator[](uint32_t idx) const {
-    return at(idx);
-  }
-
-  const CLPair& at(uint32_t idx) const {
-    if (idx >= clpairs.size())
-      return empty_pair;
-    return clpairs[idx];
-  }
-
-  bool has(const char* key) const {
-    size_t sz = clpairs.size();
-    size_t i;
-    for (i = 0; i < sz; ++i) {
-      if (clpairs[i].match(key))
-        return true;
-    }
-    return false;
-  }
-
-  bool parse(int32_t argc, char** argv) {
-    int32_t i;
-    size_t len;
-    const char* s;
-    uint32_t psr;
-    char prev_single_opt = '\0';
-
-    for (i = 0; i < argc; ++i) {
-      s = argv[i];
-      len = strlen(s);
-      if (len > 2) {
-        // --foo=bar
-        // --foo
-        if (s[0] == '-' && s[1] == '-') {
-          if (parse_pair(s + 2, len - 2)) {
-            prev_single_opt = '\0';
-            continue;
-          }
-          goto parse_arg;
-        }
-      }
-      if (len > 1) {
-        if (s[0] == '-') {
-          // 检测到单个'-'加上单个字母或数字
-          // 记录在prev_single_opt变量中
-          // 否则清空prev_single_opt变量
-          // -abc
-          // -a xxoo
-          // -b -c
-          psr = parse_single_minus(s + 1, len - 1);
-          if (psr > 0) {
-            prev_single_opt = psr == 1 ? s[1] : '\0';
-            continue;
-          }
-        }
-      }
-
-parse_arg:
-      if (prev_single_opt) {
-        char* val = copy_str_to_buf(s, len);
-        set_last_pair_value(val);
-        prev_single_opt = '\0';
-      } else {
-        char* val = copy_str_to_buf(s, len);
-        add_pair(nullptr, val);
-      }
-    }
-    return true;
-  }
+  bool parse(int32_t argc, char** argv);
 
 private:
-  bool parse_pair(const char* s, size_t l) {
-    if (!isalpha(s[0]))
-      return false;
-    size_t i;
-    char* key = nullptr;
-    char* val = nullptr;
-    for (i = 1; i < l; ++i) {
-      if (s[i] == '=') {
-        key = copy_str_to_buf(s, i);
-        val = copy_str_to_buf(s + i + 1, l - i - 1);
-        break;
-      }
-    }
-    if (i == l)
-      key = copy_str_to_buf(s, l);
-    if (l > i + 1) {
-      // '=' 后面第一个字符不能又是'='
-      if (val[0] == '=')
-        return false;
-    }
-    add_pair(key, val);
-    return true;
-  }
+  bool parse_pair(const char* s, size_t l);
 
-  uint32_t parse_single_minus(const char* s, size_t l) {
-    size_t i;
-    for (i = 0; i < l; ++i) {
-      if (!isalnum(s[i]))
-        return 0;
-    }
-    char* key;
-    for (i = 0; i < l; ++i) {
-      key = copy_str_to_buf(s + i, 1);
-      add_pair(key, nullptr);
-    }
-    return l;
-  }
+  void add_pair(const char* s1, size_t l1, const char* s2, size_t l2, bool force = false);
 
-  void add_pair(const char* s1, const char* s2) {
-    CLPair t;
-    t.key = s1;
-    t.value = s2;
-    clpairs.push_back(t);
-  }
+  void add_pair(string& key, string& val, bool force = false);
 
-  void set_last_pair_value(const char* v) {
-    clpairs.back().value = v;
-  }
+  void add_arg(const char* s, size_t l);
 
-  char* copy_str_to_buf(const char* p, uint32_t size) {
-    char* r = buffer + buf_offset;
-    if (size)
-      memcpy(r, p, size);
-    r[size] = '\0';
-    buf_offset += size + 1;
-    return r;
-  }
+  uint32_t parse_single_minus(const char* s, size_t l);
 
-private:
-  vector<CLPair> clpairs;
-  char* buffer;
-  uint32_t buf_offset = 0;
-  CLPair empty_pair;
+public:
+  map<string, string> options;
+  list<string> args;
+  map<string, string>::iterator opts_it;
+  list<string>::iterator args_it;
 };
 
-shared_ptr<CLArgs> CLArgs::parse(int32_t argc, char** argv) {
-  shared_ptr<clargs_inst> r = make_shared<clargs_inst>();
-  if (!r->parse(argc, argv))
-    return nullptr;
-  return static_pointer_cast<CLArgs>(r);
+bool clargs_inst::parse(int32_t argc, char** argv) {
+  int32_t i;
+  size_t len;
+  const char* s;
+  uint32_t psr;
+  char prev_single_opt = '\0';
+
+  for (i = 0; i < argc; ++i) {
+    s = argv[i];
+    len = strlen(s);
+    if (len > 2) {
+      // --foo=bar
+      // --foo
+      if (s[0] == '-' && s[1] == '-') {
+        if (parse_pair(s + 2, len - 2)) {
+          prev_single_opt = '\0';
+          continue;
+        }
+        goto parse_arg;
+      }
+    }
+    if (len > 1) {
+      if (s[0] == '-') {
+        // 检测到单个'-'加上单个字母或数字
+        // 记录在prev_single_opt变量中
+        // 否则清空prev_single_opt变量
+        // -abc
+        // -a xxoo
+        // -b -c
+        psr = parse_single_minus(s + 1, len - 1);
+        if (psr > 0) {
+          prev_single_opt = psr == 1 ? s[1] : '\0';
+          continue;
+        }
+      }
+    }
+
+parse_arg:
+    if (prev_single_opt) {
+      add_pair(&prev_single_opt, 1, s, len, true);
+      prev_single_opt = '\0';
+    } else {
+      add_arg(s, len);
+    }
+  }
+  opts_it = options.begin();
+  args_it = args.begin();
+  return true;
 }
 
-bool CLPair::match(const char* key) const {
-  if (key == nullptr && this->key == nullptr)
-    return true;
-  if (this->key == nullptr || key == nullptr)
+bool clargs_inst::parse_pair(const char* s, size_t l) {
+  if (!isalpha(s[0]))
     return false;
-  return strcmp(key, this->key) == 0;
+  size_t i;
+  string key;
+  string val;
+  for (i = 1; i < l; ++i) {
+    if (s[i] == '=') {
+      key.assign(s, i);
+      val.assign(s + i + 1, l - i - 1);
+      break;
+    }
+  }
+  if (i == l)
+    key.assign(s, l);
+  if (val.length() > 0) {
+    // '=' 后面第一个字符不能又是'='
+    if (val.data()[0] == '=')
+      return false;
+  }
+  add_pair(key, val);
+  return true;
 }
 
-bool CLPair::to_integer(int32_t& res) const {
-  if (value == nullptr)
-    return false;
-  char* ep;
-  res = strtol(value, &ep, 10);
-  return ep != value && ep[0] == '\0';
+uint32_t clargs_inst::parse_single_minus(const char* s, size_t l) {
+  size_t i;
+  for (i = 0; i < l; ++i) {
+    if (!isalnum(s[i]))
+      return 0;
+  }
+  for (i = 0; i < l; ++i) {
+    add_pair(s + i, 1, "", 0);
+  }
+  return l;
+}
+
+void clargs_inst::add_pair(const char* s1, size_t l1, const char* s2, size_t l2, bool force) {
+  string k, v;
+  k.assign(s1, l1);
+  v.assign(s2, l2);
+  add_pair(k, v, force);
+}
+
+void clargs_inst::add_pair(string& key, string& val, bool force) {
+  if (force || options.find(key) == options.end())
+    options[key] = val;
+}
+
+void clargs_inst::add_arg(const char* s, size_t l) {
+  string str(s, l);
+  args.push_back(str);
 }
 
 clargs_h clargs_parse(int32_t argc, char** argv) {
-  clargs_inst* r = new clargs_inst();
-  if (!r->parse(argc, argv)) {
-    delete r;
+  if (argc < 1 || argv == nullptr)
+    return 0;
+  clargs_inst* inst = new clargs_inst();
+  if (!inst->parse(argc - 1, argv + 1)) {
+    delete inst;
     return 0;
   }
-  return reinterpret_cast<intptr_t>(r);
+  return reinterpret_cast<clargs_h>(inst);
 }
 
 void clargs_destroy(clargs_h handle) {
+  if (handle)
+    delete reinterpret_cast<clargs_inst*>(handle);
+}
+
+int32_t clargs_opt_next(clargs_h handle, const char** key, const char** value) {
   if (handle == 0)
-    return;
-  delete reinterpret_cast<clargs_inst*>(handle);
-}
-
-uint32_t clargs_size(clargs_h handle) {
-  return reinterpret_cast<clargs_inst*>(handle)->size();
-}
-
-int32_t clargs_get(clargs_h handle, uint32_t idx, const char** key, const char** value) {
-  clargs_inst* inst = reinterpret_cast<clargs_inst*>(handle);
-  if (idx >= inst->size())
     return -1;
-  *key = inst->at(idx).key;
-  *value = inst->at(idx).value;
-  return 0;
-}
-
-int32_t clargs_get_integer(clargs_h handle, uint32_t idx, const char** key, int32_t* res) {
   clargs_inst* inst = reinterpret_cast<clargs_inst*>(handle);
-  if (idx >= inst->size())
-    return -1;
-  *key = inst->at(idx).key;
-  if (!inst->at(idx).to_integer(*res))
+  if (inst->opts_it == inst->options.end())
     return -2;
+  if (key)
+    *key = inst->opts_it->first.c_str();
+  if (value)
+    *value = inst->opts_it->second.c_str();
+  ++inst->opts_it;
   return 0;
+}
+
+int32_t clargs_arg_next(clargs_h handle, const char** arg) {
+  if (handle == 0)
+    return -1;
+  clargs_inst* inst = reinterpret_cast<clargs_inst*>(handle);
+  if (inst->args_it == inst->args.end())
+    return -2;
+  if (arg)
+    *arg= (*inst->args_it).c_str();
+  ++inst->args_it;
+  return 0;
+}
+
+const char* clargs_opt_get(clargs_h handle, const char* key) {
+  if (handle == 0 || key == NULL)
+    return NULL;
+  clargs_inst* inst = reinterpret_cast<clargs_inst*>(handle);
+  map<string, string>::iterator it;
+  it = inst->options.find(key);
+  if (it == inst->options.end())
+    return NULL;
+  return it->second.c_str();
+}
+
+int32_t clargs_opt_has(clargs_h handle, const char* key) {
+  if (handle == 0 || key == NULL)
+    return 0;
+  clargs_inst* inst = reinterpret_cast<clargs_inst*>(handle);
+  map<string, string>::iterator it;
+  it = inst->options.find(key);
+  if (it == inst->options.end())
+    return 0;
+  return 1;
 }
