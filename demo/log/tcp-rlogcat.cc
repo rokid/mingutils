d0d0d946fa9c8af5505e0d87bd794465b658917e
chen.zhang <chen.zhang@rokid.com>
2019-03-07 13:47:39 +0800

    retab

--- demo/log/tcp-rlogcat.cc
+++ demo/log/tcp-rlogcat.cc
@@ -14,44 +14,44 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-	int fd = socket(PF_INET, SOCK_STREAM, 0);
-	if (fd < 0)
-		return -1;
-	struct sockaddr_in addr;
-	struct hostent* hp;
-	const char* host = "localhost";
-	int32_t port = atoi(argv[1]);
-	if (argc >= 2) {
-		int n = atoi(argv[1]);
-		if (n > 0)
-			port = n;
-	}
-	hp = gethostbyname(host);
-	if (hp == NULL) {
-		printf("gethostbyname failed for host %s: %s\n", host, strerror(errno));
-		close(fd);
-		return -1;
-	}
-	printf("connect to %s:%d\n", host, port);
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	memcpy(&addr.sin_addr, hp->h_addr_list[0], sizeof(addr.sin_addr));
-	addr.sin_port = htons(port);
-	if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-		close(fd);
-		printf("socket connect failed: %s\n", strerror(errno));
-		return -1;
-	}
+  int fd = socket(PF_INET, SOCK_STREAM, 0);
+  if (fd < 0)
+    return -1;
+  struct sockaddr_in addr;
+  struct hostent* hp;
+  const char* host = "localhost";
+  int32_t port = atoi(argv[1]);
+  if (argc >= 2) {
+    int n = atoi(argv[1]);
+    if (n > 0)
+      port = n;
+  }
+  hp = gethostbyname(host);
+  if (hp == NULL) {
+    printf("gethostbyname failed for host %s: %s\n", host, strerror(errno));
+    close(fd);
+    return -1;
+  }
+  printf("connect to %s:%d\n", host, port);
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  memcpy(&addr.sin_addr, hp->h_addr_list[0], sizeof(addr.sin_addr));
+  addr.sin_port = htons(port);
+  if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    close(fd);
+    printf("socket connect failed: %s\n", strerror(errno));
+    return -1;
+  }
 
-	ssize_t c;
-	char buf[256];
-	while (true) {
-		c = read(fd, buf, sizeof(buf) - 1);
-		if (c <= 0)
-			break;
-		buf[c] = '\0';
-		printf("%s", buf);
-	}
-	close(fd);
-	return 0;
+  ssize_t c;
+  char buf[256];
+  while (true) {
+    c = read(fd, buf, sizeof(buf) - 1);
+    if (c <= 0)
+      break;
+    buf[c] = '\0';
+    printf("%s", buf);
+  }
+  close(fd);
+  return 0;
 }
