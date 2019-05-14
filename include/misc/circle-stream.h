da73a5e4823835e056984e2bd1665a69998e0285
张晨 <ming@zhangchendeMacBook-Pro.local>
2019-05-14 21:23:55 +0800

    implement CircleStream
    and use gtest to implement test case for CircleStream

--- include/misc/circle-stream.h
+++ include/misc/circle-stream.h
@@ -2,19 +2,22 @@
 
 #include <stdint.h>
 #include <string>
+#include <sys/mman.h>
 
 #define MIN_MEM_SIZE sizeof(ControlData)
 
 namespace rokid {
 
-typedef struct {
-  uint32_t front;
-  uint32_t back;
-  uint32_t space_size;
-  uint32_t unused;
-} ControlData;
+class CircleStream {
+private:
+  class ControlData {
+  public:
+    uint32_t front;
+    uint32_t back;
+    uint32_t space_size;
+    uint32_t unused;
+  };
 
-class CircleQueue {
 public:
   bool create(void* mem, uint32_t size) {
     // already initialized
@@ -43,44 +46,52 @@ public:
   }
 
   inline uint32_t capacity() const {
-    return _control->space_size;
+    return _control ? _control->space_size : 0;
   }
 
   uint32_t size() const {
-    return _control == nullptr ? 0 : _control->space_size - _remain_size;
+    return _control ? _control->space_size - _remain_size : 0;
   }
 
-  // write a data record to queue
+  // write data to stream
   int32_t write(const void* data, uint32_t length) {
-    if (_control == nullptr || _remain_size < length)
+    if (_control == nullptr)
       return -1;
-    if (length == 0)
+    if (length == 0 || _remain_size == 0)
       return 0;
-    // circle queue, if write reach memory pointer end,
+    // circle stream, if write reach memory pointer end,
     // jump to memory pointer begin, write remain data.
     uint8_t *bp = _begin + _control->back;
     uint32_t csz = _end - bp;
+    uint32_t wlen;
     if (csz >= length) {
-      memcpy(bp, data, length);
-      if (csz == length)
-        _control->back = 0;
+      if (_remain_size >= length)
+        wlen = length;
       else
-        _control->back += length;
+        wlen = _remain_size;
+      memcpy(bp, data, wlen);
+      _control->back += wlen;
     } else {
       uint32_t sz1 = csz;
       uint32_t sz2 = length - sz1;
+      uint32_t sz3;
       memcpy(bp, data, sz1);
-      memcpy(_begin, reinterpret_cast<const uint8_t*>(data) + sz1, sz2);
-      _control->back = sz2;
+      if (_control->front >= sz2)
+        sz3 = sz2;
+      else
+        sz3 = _control->front;
+      memcpy(_begin, reinterpret_cast<const uint8_t*>(data) + sz1, sz3);
+      _control->back = sz3;
+      wlen = sz1 + sz3;
     }
-    _remain_size -= length;
-    return length;
+    _remain_size -= wlen;
+    return wlen;
   }
 
   // return actual length of the data
   // or zero if no data available
   uint32_t read(void *buf, uint32_t bufsize) {
-    if (_control == nullptr || _control->space_size == _remain_size)
+    if (empty())
       return 0;
     uint32_t total_size = _control->space_size - _remain_size;
     uint32_t outsz = total_size <= bufsize ? total_size : bufsize;
@@ -110,6 +121,25 @@ public:
     return outsz;
   }
 
+  void* peek(uint32_t& sz) {
+    uint32_t count;
+    if (empty() || (count = size()) == 0) {
+      sz = 0;
+      return nullptr;
+    }
+    uint8_t *bp = _begin + _control->front;
+    uint32_t csz = _end - bp;
+    if (csz == 0) {
+      bp = _begin;
+      csz = _control->space_size;
+    }
+    if (count <= csz)
+      sz = count;
+    else
+      sz = csz;
+    return bp;
+  }
+
   // return erased data length
   uint32_t erase(uint32_t size) {
     return read(nullptr, size);
@@ -133,6 +163,37 @@ private:
   uint8_t *_end = nullptr;
   ControlData *_control = nullptr;
   uint32_t _remain_size = 0;
+};
+
+class MmapCircleStream : public CircleStream {
+public:
+  ~MmapCircleStream() { close(); }
+
+  bool create(uint32_t size) {
+    void* p = mmap(nullptr, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (p == nullptr)
+      return false;
+    auto r = CircleStream::create(p, size);
+    if (!r) {
+      munmap(p, size);
+      return false;
+    }
+    map_mem = p;
+    map_size = size;
+    return true;
+  }
+
+  void close() {
+    if (map_mem) {
+      munmap(map_mem, map_size);
+      map_mem = nullptr;
+      map_size = 0;
+    }
+  }
+
+private:
+  void* map_mem{nullptr};
+  uint32_t map_size{0};
 };
 
 } // namespace rokid
